2d85a294e8380b95fc449d8241c6824edcbaa5c5
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-10 10:12:52 -0300

    Aula 45 - Aplicando Locale
    Internacionalização nos Pipes

--- 06-pipes/src/app/app.module.ts
+++ 06-pipes/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+
+import ptBr from '@angular/common/locales/pt'; // necessário a partir do Angular v5
+import { registerLocaleData } from '@angular/common'; // necessário a partir do Angular v5
+registerLocaleData(ptBr); // necessário a partir do Angular v5
 
 @NgModule({
   declarations: [
@@ -16,7 +20,19 @@ import { CamelCasePipe } from './camel-case.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+     /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    /* SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: ptbrLocale
+    } */
+    { provide: LOCALE_ID, useValue: 'pt' } // necessário a partir do Angular v5 pt-BR nao é mais suportado
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
