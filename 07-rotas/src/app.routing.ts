8e61764818f5e62271c1a69450bbd7a446833254
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-11 12:05:17 -0300

    Aula 53 - Rotas
    Definindo e extraindo parâmetros de roteamento

--- 07-rotas/src/app.routing.ts
+++ 07-rotas/src/app.routing.ts
@@ -5,9 +5,12 @@ import { ModuleWithProviders } from "@angular/core";
 import { HomeComponent } from "./app/home/home.component";
 import { LoginComponent } from "./app/login/login.component";
 import { CursosComponent } from "./app/cursos/cursos.component";
+import { CursoDetalheComponent } from "./app/curso-detalhe/curso-detalhe.component";
+
 
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 ];
