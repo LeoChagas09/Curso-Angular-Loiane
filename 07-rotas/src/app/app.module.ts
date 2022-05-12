04a6b4971e7a41ca4c56b1a892aa57ee67bf470a
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-12 12:10:58 -0300

    Aula 60 - Rotas Filhas

--- 07-rotas/src/app/app.module.ts
+++ 07-rotas/src/app/app.module.ts
@@ -8,22 +8,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosModule } from './alunos/alunos.module';
 // import { routing } from 'src/app/app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
+    LoginComponent
     // CursosComponent,
     // CursoDetalheComponent,
     // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    CursosModule,
-    AppRoutingModule,
     FormsModule,
+    CursosModule,
+    AlunosModule,
+    AppRoutingModule,
   ],
   providers: [
     // CursosService
