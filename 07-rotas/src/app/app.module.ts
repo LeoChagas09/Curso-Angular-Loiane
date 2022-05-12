e2365aa00b43c96cf47ab3caf3da47fa9126752a
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-12 09:58:08 -0300

    Aula 58 - Rotas
    Criando um módulo de funcionalidade

--- 07-rotas/src/app/app.module.ts
+++ 07-rotas/src/app/app.module.ts
@@ -1,34 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { FormsModule } from '@angular/forms';
+import { CursosModule } from './cursos/cursos.module';
 // import { routing } from 'src/app/app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
+    CursosModule,
     AppRoutingModule,
     FormsModule,
-    AppRoutingModule,
   ],
   providers: [
-    CursosService
+    // CursosService
   ],
   bootstrap: [AppComponent]
 })
