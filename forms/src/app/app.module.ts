c5a663bddb60d2433fb7271c3ab4400e2ebc4d9d
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-20 09:09:28 -0300

    Aula 76 - Forms ( template driven)
    Módulos e FormsModule

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
