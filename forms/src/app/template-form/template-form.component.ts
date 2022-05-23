3bde2777cc6da6954087438a1bc64668311c0086
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-05-23 10:50:17 -0300

    Aula 83 - Forms (template driven)
    Refatorando CSS e mensagens de erro

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -23,4 +23,16 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  verificaValidTouched(campo: any){
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo: any){
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+
+  }
+
 }
